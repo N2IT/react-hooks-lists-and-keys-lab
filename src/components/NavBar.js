e8d14c087b257323f5eed527df7ff19c867dcb52
Tony Eder <tonyeder11@gmail.com>
2023-12-01 09:34:38 -0800

    this was challenging and fun

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    // display an <a> tag for each link here
+    // Using interpolation to sync up the # with {link} was a little tricky but got it! 
+      links.map((link) => {
+        return <a key={link} href={`#${link}`}>{link}</a>
+      })
+    }</nav>;
 }
 
 export default NavBar;
